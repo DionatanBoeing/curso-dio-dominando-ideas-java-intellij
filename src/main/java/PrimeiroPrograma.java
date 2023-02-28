758f2e8ff3d7b4c0e7f27528ae14371a3bca730f
DionatanBoeing <106771519+DionatanBoeing@users.noreply.github.com>
2023-02-28 15:14:03 -0300

    first commit

---
+++ src/main/java/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+import Model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+       /* int a = 5;
+        int b = 3;
+        System.out.println("deu certo " + (a+b));*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
